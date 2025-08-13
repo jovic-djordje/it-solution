c784e94d8fe8c048d5a26a4e2f54b3e81672c210
jovic-djordje <jovicdjordje21@gmail.com>
2025-08-13 10:59:39 +0200

    working on section six

--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -1,6 +1,7 @@
 import SectionFive from "./components/SectionFive";
 import SectionFour from "./components/SectionFour";
 import SectionOne from "./components/SectionOne";
+import SectionSix from "./components/SectionSix";
 import SectionThree from "./components/SectionThree";
 import SectionTwo from "./components/SectionTwo";
 import "./Home.style.css";
@@ -12,6 +13,7 @@ const Home = () => {
       <SectionThree />
       <SectionFour />
       <SectionFive />
+      <SectionSix />
     </>
   );
 };
